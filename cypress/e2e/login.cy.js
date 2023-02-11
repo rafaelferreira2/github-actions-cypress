148bd2e9d04d5c7b5dbc4defb57ce996fa0a982e
rafaelferreira20 <32469229+rafaelferreira2@users.noreply.github.com>
2023-02-11 15:52:11 -0300

    Update login.cy.js

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -23,7 +23,7 @@ describe('Login', () => {
     it('senha incorreta', () => {
         cy.login('qa', 'teste')
         cy.get(toast)
-            .should('have.text', 'Oops! Credenciais inválidas :(')
+            .should('have.text', 'Simulando um BUG!')
     })
 
     it('com sucesso', () => {
